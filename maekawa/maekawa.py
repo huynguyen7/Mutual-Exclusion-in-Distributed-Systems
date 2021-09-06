c2dbfa40407d8f11da22dafe5397ad1c20c707a4
Huy Nguyen <huynguyen@Huys-MacBook-Air.local>
2021-09-06 15:54:33 -0700

    cleanup

--- maekawa/maekawa.py
+++ maekawa/maekawa.py
@@ -83,34 +83,3 @@ def wait_for_votes(times=2):
                 rank = local_queue.pop(0)
                 unicast(rank, Message.REPLY)
                 voted = True
-
-
-def test():
-    # Critical section, just for testing purpose.
-    from time import sleep
-    def critical_section():
-        print(f'[Process {my_rank}] Entered the CS.')
-        sleep(1)
-        print(f'[Process {my_rank}] Exited the CS.')
-    
-    '''
-    The number of wait_for_vote() for the process if that process does not send request for Critical Section, is equal to the total number of maekawa_enter() of all other processes.
-    '''
-
-    if my_rank == 0:
-        maekawa_enter(critical_section)
-        maekawa_enter(critical_section)
-    elif my_rank == 1:
-        wait_for_votes()
-        wait_for_votes()
-        wait_for_votes()
-    elif my_rank == 2:
-        wait_for_votes()
-        wait_for_votes()
-        wait_for_votes()
-    elif my_rank == 3:
-        maekawa_enter(critical_section)
-    
-
-if __name__ == "__main__":
-    test()
